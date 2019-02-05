b5fb72ba8c736aa3a322992f402ccb1d61d9f605
Sanzhar1711 <47249605+Sanzhar1711@users.noreply.github.com>
2019-02-05 22:22:29 +0600

    Add files via upload

---
+++ Lab1_Prob3/Lab1_Prob3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Prob3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); //cчитываем количесто чисел которые будет введено
+            string[] a = Console.ReadLine().Split(); // читаем строку разделяя их по пробелам
+            List<string> number = new List<string>(); // создаем динамический массив
+
+
+            foreach (string s in a) //обращаемся к каждому элементу
+            {
+                number.Add(s); //добавляем с массива
+                number.Add(s); // еще раз добавляем
+            }
+
+            foreach (string s in number)
+            {
+                Console.Write(s + ' '); //вывод
+            }
+            Console.ReadKey();
+        }
+    }
+}
