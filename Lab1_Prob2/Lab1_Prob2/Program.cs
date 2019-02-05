27b355134210097fe4c39f880dbbb28d6b2b9640
Sanzhar1711 <47249605+Sanzhar1711@users.noreply.github.com>
2019-02-05 22:20:13 +0600

    Add files via upload

---
+++ Lab1_Prob2/Lab1_Prob2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Prob2
+{
+
+    class Student
+    {
+        public string name;
+        public string id;
+        public int yearOfStudy;
+
+        public Student(string name, string id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
